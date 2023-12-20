b9ab5e041f8029f87300ab8a20018df8aa31d016
Shreyash Kumar <kumarshreyash39@gmail.com>
2023-12-20 19:10:19 +0530

    leave server for Guest member functionality updated

--- components/provider/modal-provider.tsx
+++ components/provider/modal-provider.tsx
@@ -4,6 +4,8 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServer } from "@/components/modals/leave-server-modal";
 import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
@@ -21,6 +23,8 @@ export const ModalProvider = () => {
             <InviteModal/>
             <EditServerModal/>
             <MembersModal/>
+            <CreateChannelModal/>
+            <LeaveServer/>
         </div>
     )
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -67,6 +67,7 @@ export const ServerHeader = ({
                 )}
                 {isModerator && (
                     <DropdownMenuItem
+                        onClick={()=>onOpen("createChannel")}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Create Channel
@@ -86,6 +87,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={()=>onOpen("leaveServer",{server})}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
